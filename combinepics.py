a4ffd035949ab3bfdea3c5e4cfc0ccd7e7b74a46
ishay320 <ishay320@hotmail.com>
2022-05-05 11:00:23 +0300

    0.1

---
+++ combinepics.py
@@ -0,0 +1,39 @@
+import numpy as np
+import glob
+from PIL import Image
+import argparse
+
+def show_images(input_folder,output_folder):
+    print(input_folder + "/*.jpg")
+    images = glob.glob(input_folder + "/*.jpg")
+    counter = 0
+    set_counter = 0
+    img_array = []
+
+    not_done = True
+    while(counter < len(images)):
+        for i in range(5):
+            with open(images[counter], 'rb') as file:
+                print(images[counter])
+                pix = np.asarray(Image.open(file))
+                img_array.append(pix)
+            counter += 1
+        print(np.array(img_array).shape)
+        combined = np.mean(img_array, axis=0)
+        print(combined.shape)
+        combined_normelized = combined.astype(np.uint8)
+        Image.fromarray(combined_normelized).save(output_folder + "\\" + str(set_counter) + ".jpg")
+        img_array = []
+        set_counter += 1
+
+def argument_handling():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', required=True, type=str, help='Insert path to the folder of the files')
+    parser.add_argument('-o', required=True, type=str, help='Insert location you want to save the files')
+    args = parser.parse_args()
+    return args.i, args.o
+
+
+if __name__ == "__main__":
+    input_folder, output_folder = argument_handling()
+    show_images(input_folder,output_folder)
