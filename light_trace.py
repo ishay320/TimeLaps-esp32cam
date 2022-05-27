2b440e24f89db781acc61f4c97cba36ab15bab96
ishay320 <ishay320@hotmail.com>
2022-05-27 15:21:25 +0300

    added multyprocessing

--- light_trace.py
+++ light_trace.py
@@ -1,3 +1,4 @@
+
 from typing import List
 import glob
 import numpy as np
@@ -27,34 +28,42 @@ def readImagesToList(names: List[str]) -> list:
     return img_array
 
 def batch_process(names: List[str]):
+    if len(names) == 0:
+        return None
     main_frame = np.asarray(np.array(Image.open(names[0])).shape[0:1])
     for i, image_name in enumerate(names):
         image = Image.open(image_name)
         thresh = image_to_light_threshold(image)
         main_frame = main_frame + thresh
-        print(f"done: {i/len(names)}\tdoing: {image_name}")
-    main_frame = main_frame / len(names)
+        print(f"done: {i/len(names):.4f}\tdoing: {image_name}")
+    # main_frame = main_frame / len(names)
     return main_frame
 
 def process(input_folder, output_folder):
     print(input_folder + "/*.jpg")
     images_name = glob.glob(input_folder + "/*.jpg")
-
-    # with concurrent.futures.ThreadPoolExecutor(10) as executor:
-    #     futures = [executor.submit(batch_process, images_name[i*len(images_name):(i+1)*len(images_name)]) for i in range(10)]   # submit return a "future result".
-    #     big_sum = 0
-    #     for res in concurrent.futures.as_completed(futures):   # return each result as soon as it is completed:
-    #         r = res.result()
-    #         print("Partial sum: ", r)
-    #         big_sum += r
+    f = open("test1.txt",'w')
 
     main_frame = np.asarray(np.array(Image.open(images_name[0])).shape[0:1])
-    for i, image_name in enumerate(images_name):
-        image = Image.open(image_name)
-        thresh = image_to_light_threshold(image)
-        main_frame = main_frame + thresh
-        print(f"done: {i/len(images_name)}\tdoing: {image_name}")
-    main_frame = main_frame / len(images_name)
+    threads_num = 8
+    with concurrent.futures.ThreadPoolExecutor(threads_num) as executor:
+        list_part = len(images_name) / threads_num
+        futures = [executor.submit(batch_process, images_name[int(i * list_part) : min(int((i + 1) * list_part), len(images_name))]) for i in range(threads_num)]   # submit return a "future result".
+       
+        for res in concurrent.futures.as_completed(futures):   # return each result as soon as it is completed:
+            r = res.result()
+            if r is None:
+                continue
+            print("thread done ")
+            main_frame = main_frame + r
+
+    # main_frame = np.asarray(np.array(Image.open(images_name[0])).shape[0:1])
+    # for i, image_name in enumerate(images_name):
+    #     image = Image.open(image_name)
+    #     thresh = image_to_light_threshold(image)
+    #     main_frame = main_frame + thresh
+    #     print(f"done: {i/len(images_name)}\tdoing: {image_name}")
+    # main_frame = main_frame / len(images_name)
     return main_frame
 
 def image_to_light_threshold(image):
@@ -73,5 +82,5 @@ if __name__ == "__main__":
     image = image.astype('float64')
     image /= image.max()/1
     cv2.imshow("normal", np.array(image))
-    cv2.imwrite(f"{out_file}light.png", image)
+    cv2.imwrite(f"{out_file}light.png", image *255)
     cv2.waitKey(0)
